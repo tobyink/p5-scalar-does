f6627d1fda54a775d0a6ffe6591e09119a3ec348
Toby Inkster <mail@tobyinkster.co.uk>
2013-05-23 23:23:02 +0100

    rewrite Scalar::Does::MooseLike as a thin wrapper around Types::Standard; deprecate it in favour of Types::Standard

--- lib/Scalar/Does/MooseTypes.pm
+++ lib/Scalar/Does/MooseTypes.pm
@@ -6,57 +6,23 @@ use warnings;
 our $AUTHORITY = 'cpan:TOBYINK';
 our $VERSION   = '0.102';
 
-use Scalar::Does qw( blessed does looks_like_number -make );
+use base "Exporter::TypeTiny";
 
-sub _is_class_loaded {
-	return !!0 if ref $_[0];
-	return !!0 if !defined $_[0];
-	my $stash = do { no strict 'refs'; \%{"$_[0]\::"} };
-	return !!1 if exists $stash->{'ISA'};
-	return !!1 if exists $stash->{'VERSION'};
-	foreach my $globref (values %$stash) {
-		return !!1 if *{$globref}{CODE};
-	}
-	return !!0;
-}
-
-my @ROLES;
-my @NAMES;
 BEGIN {
-	no warnings;
-	@ROLES = (
-		make_role('Any',       sub { 1 }),
-		make_role('Item',      sub { 1 }),
-		make_role('Undef',     sub { !defined $_[0] }),
-		make_role('Defined',   sub { defined $_[0] }),
-		make_role('Bool',      sub { !defined $_[0] || $_[0] eq q() || $_[0] eq '0' || $_[0] eq '1' }),
-		make_role('Value',     sub { return unless defined $_[0]; !ref $_[0] }),
-		make_role('Ref',       sub { ref $_[0] }),
-		make_role('Str',       sub { return unless defined $_[0]; (ref(\($_[0])) eq 'SCALAR' || ref(\(my $val = $_[0])) eq 'SCALAR') }),
-		make_role('Num',       sub { return unless defined $_[0]; !ref($_[0]) && looks_like_number($_[0]) }),
-		make_role('Int',       sub { return unless defined $_[0]; !ref($_[0]) && $_[0] =~ /\A-?[0-9]+\z/ }),
-		make_role('CodeRef',   sub { ref $_[0] eq 'CODE' }),
-		make_role('RegexpRef', sub { ref $_[0] eq 'Regexp' }),
-		make_role('GlobRef',   sub { ref $_[0] eq 'GLOB' }),
-		make_role('FileHandle',sub { require IO::Detect; IO::Detect::is_filehandle($_[0]) }),
-		make_role('Object',    sub { blessed($_[0]) }),
-		make_role('ClassName', \&_is_class_loaded),
-		make_role('RoleName',  \&_is_class_loaded),
-		make_role('ScalarRef', sub { ref $_[0] eq 'SCALAR' || ref $_[0] eq 'REF' }),
-		make_role('ArrayRef',  sub { ref $_[0] eq 'ARRAY' }),
-		make_role('HashRef',   sub { ref $_[0] eq 'HASH' }),
+	my @NAMES = qw(
+		Any Item Undef Defined Bool Value Ref Str Num Int CodeRef RegexpRef
+		GlobRef FileHandle Object ClassName RoleName ScalarRef ArrayRef HashRef
 	);
-	@NAMES = map("$_", @ROLES);
-}
-
-use constant +{ map {;"$_"=>$_} @ROLES };
-use Sub::Exporter -setup => {
-	exports  => \@NAMES,
-	groups   => {
+	require constant;
+	require Types::Standard;
+	constant->import(+{ map +( $_ => "Types::Standard"->get_type($_) ), @NAMES });
+	
+	our @EXPORT_OK   = @NAMES;
+	our %EXPORT_TAGS = (
 		constants      => \@NAMES,
 		only_constants => \@NAMES,
-	},
-};
+	);
+}
 
 1;
 
@@ -64,7 +30,7 @@ __END__
 
 =head1 NAME
 
-Scalar::Does::MooseTypes - additional constants for Scalar::Does, inspired by the built-in Moose type constraints
+Scalar::Does::MooseTypes - additional constants for Scalar::Does, inspired by the built-in Moose type constraints (DEPRECATED)
 
 =head1 SYNOPSIS
 
@@ -77,11 +43,20 @@ Scalar::Does::MooseTypes - additional constants for Scalar::Does, inspired by th
     say "It's an arrayref!";
   }
 
-=head1 DESCRIPTION
+=head1 STATUS
 
-This is partially a demonstration of how to create additional roles for
-Scalar::Does, and partially included just in the hope that it would be
-useful. (The Moose type constraint system has proven quite useful.)
+This module is deprecated; use L<Types::Standard> instead:
+
+  use 5.010;
+  use Scalar::Does qw(does);
+  use Types::Standard qw(ArrayRef);
+  
+  my $var = [];
+  if (does $var, ArrayRef) {
+    say "It's an arrayref!";
+  }
+
+=head1 DESCRIPTION
 
 =head2 Constants
 
@@ -130,6 +105,8 @@ useful. (The Moose type constraint system has proven quite useful.)
 =back
 
 =head1 SEE ALSO
+
+L<Types::Standard>.
 
 L<Scalar::Does>,
 L<Moose::Util::TypeConstraints>.
